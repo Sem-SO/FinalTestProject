bbb160f15ec70a99e4075b5fb1a173288aeeed83
Sem-SO <analog9319@gmail.com>
2021-07-17 23:01:05 +0300

    add checks for registration and user

--- test_product_page.py
+++ test_product_page.py
@@ -26,8 +26,9 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_product_page()
 
 
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
     page.check_success_messege_after_adding_to_shopping_basket()
@@ -40,8 +41,9 @@ def test_guest_cant_see_success_message(browser):
     page.check_success_message_before_adding_to_shopping_basket()
 
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
     page.chech_success_message_disappeared_after_adding_to_shopping_basket()
